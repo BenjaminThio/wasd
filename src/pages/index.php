e0e380d6f73d43d13d84acb615ba63d953469214
Benjamin Thio <74221883+BenjaminThio@users.noreply.github.com>
2026-07-01 16:34:18 +0800

    Initial commit

---
+++ src/pages/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <main>
+            <div>Hello Mum!</div>
+        </main>
+    </body>
+</html>
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Starting PHP Server
+start http://localhost:3000/src/pages/index.php
+php -S localhost:3000
