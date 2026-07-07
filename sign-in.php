dbee4f8a101af8b6116edb98ef437408895ff4b9
teehueleng123 <teehueleng123@gmail.com>
2026-07-07 23:48:52 +0800

    Task
    sign-in page done.

---
+++ sign-in.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+
+<body class="page-container">
+
+    <video autoplay muted loop playsinline class="video-bg">
+        <source src="./public/gif.mp4" type="video/mp4">
+    </video>
+
+    <div class="box font">
+        <div class="welcome">WELCOME</div>
+
+        <div class="input-group">Your wishlist, cart and reviews are waiting where you left them.</div>
+
+        <div class="input-group">
+            <label class="field-label" for="email">EMAIL ADDRESS:</label>
+            <input type="email" id="email" class="field-input" placeholder="Type here...">
+        </div>
+
+        <div class="input-group">
+            <label class="field-label" for="password">PASSWORD:</label>
+            <input type="password" id="password" class="field-input" placeholder="Type here...">
+        </div>
+
+        <div class="remember-me">
+            <input type="checkbox" id="remember-box">
+            <label for="remember-box">REMEMBER ME</label>
+        </div>
+
+        <div>
+            <button class="login-button font">LOGIN</button>
+        </div>
+
+        <div class="social-container">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-google"></i>
+            <i class="fa-brands fa-discord"></i>
+        </div>
+
+        <div class="signup-link">
+            New to WASD? <a href="#">SIGN UP</a>
+        </div>
+
+
+</body>
+
+</html>
