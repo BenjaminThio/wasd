1c4a231f543a73527c2811b11d25d542c3e6cc23
Benjamin Thio <74221883+BenjaminThio@users.noreply.github.com>
2026-07-16 02:19:19 +0800

    Create example SQL query in test page.

--- src/app/test/index.php
+++ src/app/test/index.php
@@ -3,5 +3,13 @@
         require __DIR__ . '/../../lib/Database.php';
 
         $database = new Database();
+
+        $database->query(
+            'CREATE TABLE STUDENT (
+                StudentId int PRIMARY KEY,
+                StudentName varchar(255) NOT NULL,
+                Address varchar(255)
+            )'
+        );
     ?>
 </div>
